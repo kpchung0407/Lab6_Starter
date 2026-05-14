50df2b0277edd12c5c8d802e0c3e1bb55246e104
kpchung0407 <uyeong47@gmail.com>
2026-05-13 19:25:20 -0700

    change for 5 star

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -141,7 +141,7 @@ class RecipeCard extends HTMLElement {
   		<p class="organization">${data.organization}</p>
   		<div class="rating">
     	<span>${data.rating}</span>
-    	<img src="/assets/images/icons/${data.rating}-star.svg" alt="${data.rating} stars">
+    	<img src="assets/images/icons/${data.rating}-star.svg" alt="${data.rating} stars">
     <span> (${data.numRatings})</span>
   </div>
   <time>${data.lengthTime} </time>
@@ -153,5 +153,5 @@ class RecipeCard extends HTMLElement {
 }
 
 // A8. TODO - Define the Class as a customElement so that you can create
-//           'recipe-card' elements
+//           'recipe-card' elementsa
 customElements.define('recipe-card', RecipeCard);
